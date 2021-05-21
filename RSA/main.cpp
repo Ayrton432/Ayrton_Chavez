b4929a4f86888d107382c3113a8148126877058a
Ayrton432 <74332373+Ayrton432@users.noreply.github.com>
2021-05-21 10:57:08 -0500

    Update main.cpp

--- RSA/main.cpp
+++ RSA/main.cpp
@@ -4,12 +4,16 @@
 using namespace std;
 
 int main() {
-  RSA Emisor;
+  RSA emisor;
+  /*
+  Emisor emisor;
+  Receptor receptor;
+  */
   int mensaje = 20;
   cout << "mensaje: " << mensaje << endl;
-  int m_cifrado = Emisor.cifrado(mensaje);
+  int m_cifrado = emisor.cifrado(mensaje);
   cout << "mensaje cifrado: " << m_cifrado << endl;
   cout << endl;
-  int m_descifrado = Emisor.descifrado(m_cifrado);
+  int m_descifrado = emisor.descifrado(m_cifrado);
   cout << "mensaje descifrado: " << m_descifrado << endl;;
 }
